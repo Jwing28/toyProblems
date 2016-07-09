14b5eae86ede392eaf8a65970a1e2a7764c4d283
Jwing28 <jonathan.wing.lee@gmail.com>
2016-07-09 11:21:23 -0700

    [update] update RPS comments

--- RPS.js
+++ RPS.js
@@ -7,17 +7,22 @@ function rockPaperPermutation (roundCount) {
   if(roundCount === 0){
     return [];
   }
-  
+  //using recursion, concat and a for loop
+  //able to build up a string of the possible combinations
+
   var recurse = function (currentPlay) {
+    //base case
     if(currentPlay.length === roundCount){
       result.push(currentPlay);
       return;  
     }else{
       for (var i = 0; i < plays.length;i++){
+        //recursive case
         recurse(currentPlay.concat(plays[i]));
       }
     }
   };
+  //note start with empty string
   recurse("");
   
   return result;
